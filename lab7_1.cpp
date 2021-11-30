4e4b3b6459dc4113dd200f52418f392edff1b96b
PHANAGUS <phanarin03072002@gmail.com>
2021-11-30 15:04:53 +0700

    lab7_1.cpp completed

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -3,39 +3,56 @@
 
 using namespace std;
 
-string func1(string x){
+string reverse(string x)
+{
 	int i = 0, L = x.size();
 	string y = "";
-	while(i < L){
+	while(i < L)
+	{
 		y += x[L-i-1];
 		i++;
 	}
 	return y;
 }
 
-string func2(string x){
+string toupper(string x)
+{
 	int i = 0, L = x.size();
 	string y = "";
-	while(i < L){
+	while(i < L)
+	{
 		y += toupper(x[i]);
 		i++;
 	}
-	return y;	
+	return y;
 }
 
-string func3(string x){
+string tolower(string x)
+{
 	int i = 0, L = x.size();
 	string y = "";
-	while(i < L){
+	while(i < L)
+	{
 		y += tolower(x[i]);
 		i++;
 	}
-	return y;	
+	return y;
 }
 
 int main(){
-    cout >> "Input text: "
-    cout >> "Reversed text: "
-    cout >> "Palindrome: "
+	string text;
+    cout << "Input text: ";
+	cin >> text;
+	string revtext = reverse(text);
+    cout << "Reversed text: " << revtext << endl;
+    cout << "Palindrome: ";
+	if(toupper(text) == toupper(revtext))
+	{
+		cout << "Yes";
+	}
+	else
+	{
+		cout << "No";
+	}
     return 0;
 }
