a261314f1b7a626f582d75ce389ed2230c934248
PHANAGUS <phanarin03072002@gmail.com>
2021-11-30 15:28:52 +0700

    lab7_2.cpp completed

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -2,12 +2,38 @@
 
 using namespace std;
 
-int main(){
-	
-	int i = 0, P[5000], k, result;
-	while(i < 5000){
-		P[i] = 2*i+1;
-		i++;
+int main()
+{
+	int j=0, P[5000];
+	while(j < 5000)
+	{
+		P[j] = 2*j+1;
+		j++;
+	}
+
+	int k;
+	cout << "Please input k: ";
+	cin >> k;
+	if(k > 0)
+	{
+		int i=0, result=0;
+		while(i < 5000)
+		{
+			if(P[i]%k == 0)
+			{
+				result = result - P[i];
+			}
+			else
+			{
+				result = result + P[i];
+			}
+			i++;
+		}
+		cout << "Result = " << result;
+	}
+	else
+	{
+		cout << "Invalid input!!!";
 	}
 	
 	return 0;
